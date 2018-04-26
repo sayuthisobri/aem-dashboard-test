539637d4da06b43b8f7c9de46f17d9a365031215
Muhammad Sayuthi <sayuthisobri@gmail.com>
2018-04-26 17:37:41 +0800

    rebuild

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-ng build --prod --output-path docs --base-href aem-dashboard-test
+ng build --prod --output-path docs --base-href \/aem-dashboard-test\/
 cp ./docs/index.html ./docs/404.html
-
